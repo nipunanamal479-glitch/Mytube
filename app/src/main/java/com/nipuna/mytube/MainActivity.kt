1564b6142c7aa2e24511a1111aa96730fe33b431
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-07-11 00:58:25 +0530

    Update MainActivity.kt

--- app/src/main/java/com/nipuna/mytube/MainActivity.kt
+++ app/src/main/java/com/nipuna/mytube/MainActivity.kt
@@ -1,55 +1,31 @@
 package com.nipuna.mytube
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.inputmethod.EditorInfo
-import android.widget.EditText
-import android.widget.Toast
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
-    private val API_KEY = BuildConfig.YOUTUBE_API_KEY
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val searchBox = findViewById<EditText>(R.id.searchBox)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        webView = WebView(this)
+        setContentView(webView)
 
-        searchBox.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                val query = searchBox.text.toString()
-                if (query.isNotBlank()) searchYoutube(query, recyclerView)
-                true
-            } else false
-        }
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
 
-        searchYoutube("trending sri lanka", recyclerView)
+        webView.loadUrl("https://m.youtube.com")
     }
 
-    private fun searchYoutube(query: String, recyclerView: RecyclerView) {
-        lifecycleScope.launch {
-            try {
-                val response = RetrofitClient.instance.searchVideos(
-                    query = query,
-                    apiKey = API_KEY
-                )
-                val adapter = VideoAdapter(response.items) { video ->
-                    val intent = Intent(this@MainActivity, PlayerActivity::class.java)
-                    intent.putExtra("VIDEO_ID", video.id.videoId)
-                    startActivity(intent)
-                }
-                recyclerView.adapter = adapter
-            } catch (e: Exception) {
-                Toast.makeText(this@MainActivity, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
-            }
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
         }
     }
 }
