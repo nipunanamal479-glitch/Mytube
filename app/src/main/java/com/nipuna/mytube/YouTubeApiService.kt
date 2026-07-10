e4cbf48000a83997bbbe401b372ab286147aa8cf
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-07-11 02:59:38 +0530

    Update YouTubeApiService.kt

--- app/src/main/java/com/nipuna/mytube/YouTubeApiService.kt
+++ app/src/main/java/com/nipuna/mytube/YouTubeApiService.kt
@@ -1,15 +1,18 @@
 package com.nipuna.mytube
 
+import com.nipuna.mytube.model.SearchResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface YouTubeApiService {
+
     @GET("search")
     suspend fun searchVideos(
         @Query("part") part: String = "snippet",
         @Query("q") query: String,
         @Query("type") type: String = "video",
-        @Query("maxResults") maxResults: Int = 25,
+        @Query("maxResults") maxResults: Int = 20,
         @Query("key") apiKey: String
-    ): SearchResponse
+    ): Response<SearchResponse>
 }
