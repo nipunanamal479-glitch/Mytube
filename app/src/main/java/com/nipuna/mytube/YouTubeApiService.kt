85d44f1d1d0f0cc28e0d02e430db74a04f092914
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-07-10 22:52:10 +0530

    Create YouTubeApiService.kt

---
+++ app/src/main/java/com/nipuna/mytube/YouTubeApiService.kt
@@ -0,0 +1,15 @@
+package com.nipuna.mytube
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface YouTubeApiService {
+    @GET("search")
+    suspend fun searchVideos(
+        @Query("part") part: String = "snippet",
+        @Query("q") query: String,
+        @Query("type") type: String = "video",
+        @Query("maxResults") maxResults: Int = 25,
+        @Query("key") apiKey: String
+    ): SearchResponse
+}
