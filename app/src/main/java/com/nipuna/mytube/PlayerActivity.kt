ee6e88fdd6fcedcdf695e596cdbbe98940d8774b
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-07-11 10:02:16 +0530

    Update PlayerActivity.kt

--- app/src/main/java/com/nipuna/mytube/PlayerActivity.kt
+++ app/src/main/java/com/nipuna/mytube/PlayerActivity.kt
@@ -42,6 +42,7 @@ class PlayerActivity : AppCompatActivity() {
             mediaPlaybackRequiresUserGesture = false
             loadWithOverviewMode = true
             useWideViewPort = true
+            userAgentString = "Mozilla/5.0 (Linux; Android 13; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
         }
 
         playerWebView.webViewClient = object : WebViewClient() {
