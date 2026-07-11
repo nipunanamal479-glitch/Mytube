1e1451f20ef2f4ad6571c3975d6c05006a5d6183
nipunanamal479-glitch <nipunanamal479@gmail.com>
2026-07-11 09:49:44 +0530

    Update YouTubeApiService.kt

--- app/src/main/java/com/nipuna/mytube/YouTubeApiService.kt
+++ app/src/main/java/com/nipuna/mytube/YouTubeApiService.kt
@@ -1,6 +1,7 @@
 package com.nipuna.mytube
 
 import com.nipuna.mytube.model.SearchResponse
+import com.nipuna.mytube.model.VideoListResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -15,4 +16,13 @@ interface YouTubeApiService {
         @Query("maxResults") maxResults: Int = 20,
         @Query("key") apiKey: String
     ): Response<SearchResponse>
+
+    @GET("videos")
+    suspend fun getTrendingVideos(
+        @Query("part") part: String = "snippet",
+        @Query("chart") chart: String = "mostPopular",
+        @Query("regionCode") regionCode: String = "LK",
+        @Query("maxResults") maxResults: Int = 20,
+        @Query("key") apiKey: String
+    ): Response<VideoListResponse>
 }
